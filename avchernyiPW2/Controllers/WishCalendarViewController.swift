b77791f23273f114163c69dfad4dfb9a8a58425f
Алексей Чёрный <avea05.alex@yandex.ru>
2024-12-04 22:52:27 +0300

    HW4: Point 4 has been completed

--- avchernyiPW2/Controllers/WishCalendarViewController.swift
+++ avchernyiPW2/Controllers/WishCalendarViewController.swift
@@ -9,5 +9,71 @@ import UIKit
 
 //MARK: - WishCalendarViewController
 final class WishCalendarViewController: UIViewController {
+    // MARK: - Constants
+    enum Constants {
+        static let contentInset: UIEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        static let collectionTop: CGFloat = 40
+    }
     
+    // MARK: - Variables
+    private let collectionView: UICollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewFlowLayout()
+    )
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        configureUI()
+    }
+    
+    // MARK: - Private functions
+    private func configureUI() {
+        view.backgroundColor = .cyan
+        configureCollection()
+    }
+    
+    private func configureCollection() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .white
+        collectionView.alwaysBounceVertical = true
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.contentInset = Constants.contentInset
+        
+        /* Temporary line */
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+        view.addSubview(collectionView)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: Constants.collectionTop),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension WishCalendarViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowAutoLayout
+extension WishCalendarViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width - 10, height: 100)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Cell tapped at index \(indexPath.item)")
+    }
 }
